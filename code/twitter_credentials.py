19d6772359112d025762683d7436c2fcfa55702d
Toyesh SINGH <toyesh19@gmail.com>
2019-11-25 22:03:05 +0530

    Delete twitter_credentials.py

--- code/twitter_credentials.py
+++
@@ -1,5 +0,0 @@
-# Variables that contains the user credentials to access Twitter API 
-ACCESS_TOKEN = "1194097764998627328-RvGTv4uEQxGyYl7KJRZX0K9MOSNuWi"
-ACCESS_TOKEN_SECRET = "nNOOVVvdu5VFYwEBq5m8i5IIcpbsBMF3mD5SG6sDOvrzv"
-CONSUMER_KEY = "a2aJ0OuwatZEmXSpAFco9Hdgp"
-CONSUMER_SECRET = "xONGDfSeHXbcHx33eX7JwWGY5qPoO8tpzN0nizSl00fhwIxOTq"
